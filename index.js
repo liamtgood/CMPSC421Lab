85f226268827a45d1ac16db405a9e0a2f2a3d88d
liamtgood <liamgood83@gmail.com>
2025-04-18 13:13:50 -0400

    some new stuff that doesnt work

--- index.js
+++ index.js
@@ -8,22 +8,37 @@ const swaggerJSDoc = require('swagger-jsdoc'); // Path to your Swagger JSON file
 
 const app = express();
 const host = '0.0.0.0'
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(bodyParser.json());
 
 // MongoDB Connection
+const dbUsername = process.env.MONGO_INITDB_ROOT_USERNAME || 'root';
+const dbPassword = process.env.MONGO_INITDB_ROOT_PASSWORD || 'examplepassword';
+const dbHost = process.env.MONGO_HOST || 'mongodb';
+const dbPort = process.env.MONGO_PORT || '27017';
+const dbName = process.env.MONGO_DB_NAME || 'mydatabase';
 //mongodb://localhost/mydatabase', {
 //password: Ltg220929$$
 //const uri = "mongodb+srv://liamgood83:Ltg220929$$@421lab.cvuv0jp.mongodb.net/?retryWrites=true&w=majority&appName=421Lab";
 const uri = "mongodb://host.docker.internal:27017/mydatabase";
-mongoose.connect(uri);
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open', () => {
-  console.log('Connected to MongoDB');
-});
+
+async function connectToMongoDB() {
+  try {
+    mongoose.connect("mongodb://mongodb:27017/", {
+      dbName: 'mydatabase',
+    });
+    const db = mongoose.connection;
+    db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+    db.once('open', () => {
+      console.log('Connected to MongoDB');
+    });
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error);
+  }
+}
+
 
 //swagger connection
 const swaggerOptions = {
@@ -83,6 +98,7 @@ app.listen(PORT,host, () => {
   console.log(`Order system is running on port ${PORT}`);
   console.log(`Server running at http://localhost:${PORT}`);
   console.log(`Swagger UI available at http://localhost:${PORT}/api-docs`);
+  connectToMongoDB();
 });
 
 //const Item = mongoose.model('Item',itemSchema);
